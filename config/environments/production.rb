07ae6d4cc23dba2fbd93c24cf483dc98696d9cd6
MIlind Singh <milindsingh84@gmail.com>
2015-07-19 14:16:24 -0700

    added changes for showing background image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,24 +1,28 @@
 RubyVideoConvertor::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # Code is not reloaded between requests
-  config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+# all must be true==========================
+
+  # Code is not reloaded between requests
+  config.cache_classes = true
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+# all must be true==========================
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
